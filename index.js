4ba04a0334dded6d625d3ffa5870bc9ad6a0cfb4
NICK MUGO NJURU <nicknjuru13@gmail.com>
2023-07-08 09:41:51 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string){
+    return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(string){
+    return console.log(string.toUpperCase("hello"));
+}
+function logWhisper(string){
+    return console.log(string.toLowerCase("HELLO"));
+}
+function response(string){
+    return response
+}
+let string="good"
+function sayHiToGrandma(string) {
+    if (string.toUpperCase === string) {
+      response="YES INDEED!";
+    } elseif(string.toLowerCase===string);{
+      response= "I can't hear you!";
+    }{elseif(string="Let's have dinner together!")
+    response="I would love to!";
+    }}
+    console.log(response)
